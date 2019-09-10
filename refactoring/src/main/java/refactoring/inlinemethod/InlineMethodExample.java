dd7172202a23bfc73b0a44f9f0792a87bdc8b072
dev <dev@wsx09.mfti>
2019-09-10 14:42:39 +0200

    Inline Method - refactored

--- refactoring/src/main/java/refactoring/inlinemethod/InlineMethodExample.java
+++ refactoring/src/main/java/refactoring/inlinemethod/InlineMethodExample.java
@@ -3,11 +3,7 @@ package refactoring.inlinemethod;
 public class InlineMethodExample {
     
     int getRating() {
-        return (moreThanFiveLateDeliveries()) ? 2 : 1;
-    }
-    
-    boolean moreThanFiveLateDeliveries() {
-        return _numberOfLateDeliveries > 5;
+        return (_numberOfLateDeliveries > 5) ? 2 : 1;
     }
     
 }
