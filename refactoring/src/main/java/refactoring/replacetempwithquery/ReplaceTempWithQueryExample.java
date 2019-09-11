e42ab3fb13ea9a056a4ecb280276363a9ebb7810
dev <dev@wsx09.mfti>
2019-09-11 10:23:07 +0200

    Replace Temp With Query - refactoring

--- refactoring/src/main/java/refactoring/replacetempwithquery/ReplaceTempWithQueryExample.java
+++ refactoring/src/main/java/refactoring/replacetempwithquery/ReplaceTempWithQueryExample.java
@@ -5,11 +5,14 @@ public class ReplaceTempWithQueryExample {
     private double _itemPrice;
     
     public double getBasePrice() {
-        double basePrice = _quantity * _itemPrice;
-        if (basePrice > 1000)
-            return basePrice * 0.95;
+        if (basePrice() > 1000)
+            return basePrice() * 0.95;
         else
-            return basePrice * 0.98;
+            return basePrice() * 0.98;
+    }
+    
+    public double basePrice() {
+        return _quantity * _itemPrice;
     }
 
 }
