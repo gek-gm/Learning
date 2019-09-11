985098cbce9bfc35d2a2deb4e07f9ba60d6f3a19
dev <dev@wsx09.mfti>
2019-09-11 14:40:50 +0200

    Replace Method with Method object - initial

---
+++ refactoring/src/main/java/refactoring/replacemethodwithmethodobject/ReplaceMethodWithMethodObjectExample.java
@@ -0,0 +1,16 @@
+package refactoring.replacemethodwithmethodobject;
+
+public class ReplaceMethodWithMethodObjectExample {
+    class Account {
+        int gamma (int inputVal, int quantity, int yearToDate) {
+        int importantValue1 = (inputVal * quantity) + delta();
+        int importantValue2 = (inputVal * yearToDate) + 100;
+        if ((yearToDate - importantValue1) > 100)
+        importantValue2 -= 20;
+        int importantValue3 = importantValue2 * 7;
+        // and so on.
+        return importantValue3 - 2 * importantValue1;
+        }
+    }
+    
+}
