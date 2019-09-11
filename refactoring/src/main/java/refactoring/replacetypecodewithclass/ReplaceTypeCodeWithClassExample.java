3b3872cba43d3a528d9df4171051e966e2055b1c
dev <dev@wsx09.mfti>
2019-09-11 17:00:47 +0200

    Replace Type Code with Class - refactoring

--- refactoring/src/main/java/refactoring/replacetypecodewithclass/ReplaceTypeCodeWithClassExample.java
+++ refactoring/src/main/java/refactoring/replacetypecodewithclass/ReplaceTypeCodeWithClassExample.java
@@ -3,23 +3,42 @@ package refactoring.replacetypecodewithclass;
 public class ReplaceTypeCodeWithClassExample {
     
     class Person {
-        public static final int O = 0;
-        public static final int A = 1;
-        public static final int B = 2;
-        public static final int AB = 3;
-        private int _bloodGroup;
+        private BloodGroup _bloodGroup;
         
-        public Person (int bloodGroup) {
+        public Person (BloodGroup bloodGroup) {
             _bloodGroup = bloodGroup;
         }
         
-        public void setBloodGroup(int arg) {
-            _bloodGroup = arg;
+        public void setBloodGroup(BloodGroup bloodGroup) {
+            _bloodGroup = bloodGroup;
         }
         
-        public int getBloodGroup() {
+        public BloodGroup getBloodGroup() {
             return _bloodGroup;
         }
     }
     
+    enum BloodGroup {
+        ZERO(0),
+        A(1),
+        B(2),
+        AB(3);
+        
+        private final int _code;
+
+        BloodGroup(int code) {
+            _code = code;
+        }
+        
+        public int getCode() {
+            return _code;
+        }
+        
+        public BloodGroup getByCode(int code) {
+            for (BloodGroup b : BloodGroup.values())
+                if (b.getCode() == code)
+                    return b;
+            return null;
+        }
+    }
 }
