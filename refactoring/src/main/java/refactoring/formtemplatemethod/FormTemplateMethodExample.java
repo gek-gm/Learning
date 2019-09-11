3a9356808312d2e9d5cef6c1d1426c2cd7a3cdfb
dev <dev@wsx09.mfti>
2019-09-11 09:08:32 +0200

    Form Template Method - refactoring

--- refactoring/src/main/java/refactoring/formtemplatemethod/FormTemplateMethodExample.java
+++ refactoring/src/main/java/refactoring/formtemplatemethod/FormTemplateMethodExample.java
@@ -1,42 +1,113 @@
 package refactoring.formtemplatemethod;
 
 import java.util.Enumeration;
+import java.util.Vector;
 
 public class FormTemplateMethodExample {
+    
+    public abstract class Statement{
+        
+        public String value(Customer aCustomer) {
+            Enumeration<Rental> rentals = aCustomer.getRentals().elements();
+            String result = headerString(aCustomer);
+            // show figures for this rental
+            while (rentals.hasMoreElements()) {
+                Rental each = rentals.nextElement();
+                result += eachRentalString(each);
+            }
+            result += footerString(aCustomer);
+            return result;
+        }
+        
+        abstract String headerString(Customer aCustomer);
+        abstract String eachRentalString(Rental aRental);
+        abstract String footerString(Customer aCustomer);
+        
+    }
+    
+    public class HtmlStatement extends Statement{
+        
+        @Override
+        public String headerString(Customer aCustomer) {
+            return "<H1>Rentals for <EM>" + aCustomer.getName() + "</EM></H1><P>\n";
+        }
+        
+        @Override
+        public String eachRentalString(Rental aRental) {
+            return aRental.getMovie().getTitle()+ ": " + String.valueOf(aRental.getCharge()) + "<BR>\n";
+        }
+        
+        @Override
+        public String footerString(Customer aCustomer) {
+            return "Amount owed is " + String.valueOf(aCustomer.getTotalCharge()) + "\n" +
+            "You earned " + String.valueOf(aCustomer.getTotalFrequentRenterPoints()) + 
+            " frequent renter points";
+        }
+    }
+
+    public class TextStatement extends Statement{
+        
+        @Override
+        public String headerString(Customer aCustomer) {
+            return "Rental Record for " + aCustomer.getName() + "\n";
+        }
+        
+        @Override
+        public String eachRentalString(Rental aRental) {
+            return "\t" + aRental.getMovie().getTitle()+ "\t" + String.valueOf(aRental.getCharge()) + "\n";
+        }
+        
+        @Override
+        public String footerString(Customer aCustomer) {
+            return "Amount owed is " + String.valueOf(aCustomer.getTotalCharge()) + "\n" +
+            "You earned " + String.valueOf(aCustomer.getTotalFrequentRenterPoints()) + 
+            " frequent renter points";
+        }
+    }
+    
+    
+    
+    
+    public class Rental{
+        public Movie getMovie() {
+            return new Movie();
+        }
+        public double getCharge() {
+            return 0.0;
+        }
+    }
+    
+    public class Movie{
+        public String getTitle() {
+            return "title";
+        }
+    }
    
-    public static class Customer{
+    public class Customer{
+        
+        private final Vector<Rental> _rentals = new Vector<>();
         
         public String statement() {
-            Enumeration rentals = _rentals.elements();
-            String result = "Rental Record for " + getName() + "\n";
-            while (rentals.hasMoreElements()) {
-                Rental each = (Rental) rentals.nextElement();
-                //show figures for this rental
-                result += "\t" + each.getMovie().getTitle()+ "\t" +
-                String.valueOf(each.getCharge()) + "\n";
-            }
-            //add footer lines
-            result += "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
-            result += "You earned " +
-            String.valueOf(getTotalFrequentRenterPoints()) + " frequent renter points";
-            return result;
+            return new TextStatement().value(this);
         }
         
         public String htmlStatement() {
-            Enumeration rentals = _rentals.elements();
-            String result = "<H1>Rentals for <EM>" + getName() + "</EM></H1><P>\n";
-            while (rentals.hasMoreElements()) {
-                Rental each = (Rental) rentals.nextElement();
-                //show figures for each rental
-                result += each.getMovie().getTitle()+ ": " +
-                String.valueOf(each.getCharge()) + "<BR>\n";
-            }
-            //add footer lines
-            result += "<P>You owe <EM>" + String.valueOf(getTotalCharge()) + "</EM><P>\n";
-            result += "On this rental you earned <EM>" +
-            String.valueOf(getTotalFrequentRenterPoints()) + "</EM> frequent renter points<P>";
-            return result;
+            return new HtmlStatement().value(this);
         }
         
+        public String getName() {
+            return "name";
+        }
+        
+        public Vector<Rental> getRentals() {
+            return _rentals;
+        }
+        public double getTotalCharge() {
+            return 2.0;
+        }
+        public int getTotalFrequentRenterPoints() {
+            return 5;
+        }
     }
+    
 }
