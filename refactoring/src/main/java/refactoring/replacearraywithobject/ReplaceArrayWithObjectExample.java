0d508021384a2d6a62a23eddf16e0c61d6d0dad1
dev <dev@wsx09.mfti>
2019-09-12 09:59:41 +0200

    Replace array with object - initial

---
+++ refactoring/src/main/java/refactoring/replacearraywithobject/ReplaceArrayWithObjectExample.java
@@ -0,0 +1,10 @@
+package refactoring.replacearraywithobject;
+
+public class ReplaceArrayWithObjectExample {
+    public void performance() {
+        String[] row = new String[3];
+        row[0] = "Liverpool";
+        row[1] = "15";
+        row[2] = "7";
+    }
+}
