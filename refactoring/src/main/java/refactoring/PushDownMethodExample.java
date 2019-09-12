6f12c180db83483d940beb00fb4d559e773ceb21
dev <dev@wsx09.mfti>
2019-09-12 16:50:58 +0200

    Push Down Method/Push Down Field - refactoring

--- refactoring/src/main/java/refactoring/PushDownMethodExample.java
+++ refactoring/src/main/java/refactoring/PushDownMethodExample.java
@@ -3,16 +3,15 @@ package refactoring;
 public class PushDownMethodExample {
     
     class Employee{
+        
+    }
+    
+    class SalesMan extends Employee{
         private final int _quota = 10;
         
         public int getQuota() {
             return _quota;
         }
-        
-    }
-    
-    class SalesMan extends Employee{
-        
     }
     
     class Engineer extends Employee{
