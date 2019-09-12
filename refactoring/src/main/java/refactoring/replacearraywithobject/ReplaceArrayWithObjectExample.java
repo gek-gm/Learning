3cf1c71bfba322c7bce06d76dd8e6c34ed945135
dev <dev@wsx09.mfti>
2019-09-12 10:04:04 +0200

    Replace array with object - refactoring

--- refactoring/src/main/java/refactoring/replacearraywithobject/ReplaceArrayWithObjectExample.java
+++ refactoring/src/main/java/refactoring/replacearraywithobject/ReplaceArrayWithObjectExample.java
@@ -2,9 +2,39 @@ package refactoring.replacearraywithobject;
 
 public class ReplaceArrayWithObjectExample {
     public void performance() {
-        String[] row = new String[3];
-        row[0] = "Liverpool";
-        row[1] = "15";
-        row[2] = "7";
+        Performance performance = new Performance();
+        performance.setName("Liverpool");
+        performance.setWins(15);
+        performance.setLosses(7);
+    }
+    
+    public class Performance {
+        private String _name;
+        private int _wins;
+        private int _losses;
+        
+        public String getName() {
+            return _name;
+        }
+        
+        public void setName(String name) {
+            _name = name;
+        }
+        
+        public int getWins() {
+            return _wins;
+        }
+        
+        public void setWins(int wins) {
+            _wins = wins;
+        }
+        
+        public int getLosses() {
+            return _losses;
+        }
+        
+        public void setLosses(int losses) {
+            _losses = losses;
+        }
     }
 }
