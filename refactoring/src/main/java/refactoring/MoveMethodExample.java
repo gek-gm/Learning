41b27b1810a0f813152aa721a0234bd7df2b9a30
dev <dev@wsx09.mfti>
2019-09-12 10:52:01 +0200

    Move Method - refactoring

--- refactoring/src/main/java/refactoring/MoveMethodExample.java
+++ refactoring/src/main/java/refactoring/MoveMethodExample.java
@@ -1,5 +1,32 @@
 package refactoring;
 
 public class MoveMethodExample {
-
+    class Account {
+        private AccountType _type;
+        private int _daysOverdrawn;
+        
+        double overdraftCharge() {
+            return _type.overdraftCharge(_daysOverdrawn);
+        }
+        
+        double bankCharge() {
+            double result = 4.5;
+            if (_daysOverdrawn > 0) result += overdraftCharge();
+                return result;
+        }
+    }
+    
+    abstract class AccountType {
+        double overdraftCharge(int daysOverdrawn) {
+            if (isPremium()) {
+                double result = 10;
+                if (daysOverdrawn > 7) result += (daysOverdrawn - 7) * 0.85;
+                    return result;
+            }
+            else 
+                return daysOverdrawn * 1.75;
+        }
+        abstract boolean isPremium();
+    }
 }
+    
