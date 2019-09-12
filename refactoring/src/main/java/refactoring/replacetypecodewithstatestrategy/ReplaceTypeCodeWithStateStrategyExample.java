fb286173377058527184290ad6cf277038fee893
dev <dev@wsx09.mfti>
2019-09-12 09:44:06 +0200

    Replace Type Code With Strategy - refactoring

--- refactoring/src/main/java/refactoring/replacetypecodewithstatestrategy/ReplaceTypeCodeWithStateStrategyExample.java
+++ refactoring/src/main/java/refactoring/replacetypecodewithstatestrategy/ReplaceTypeCodeWithStateStrategyExample.java
@@ -3,31 +3,83 @@ package refactoring.replacetypecodewithstatestrategy;
 public class ReplaceTypeCodeWithStateStrategyExample {
     
     class Employee {
-        private final int _type;
-        private final int _monthlySalary;
-        private final int _commission;
-        private final int _bonus;
+        private EmployeeType _type;
+        
+        public Employee (int employeeTypeCode) {
+            setType(employeeTypeCode);
+        }
+        
+        public void setType(int typeCode) {
+            _type = EmployeeType.createType(typeCode);
+        }
+        
+        int payAmount() {
+            return _type.payAmount();
+        }
+    }
+    
+    static abstract class EmployeeType{
         static final int ENGINEER = 0;
         static final int SALESMAN = 1;
         static final int MANAGER = 2;
         
-        public Employee (int type) {
-            _type = type;
-            _monthlySalary = 100;
-            _commission = 100;
-            _bonus = 100;
-        }
-        int payAmount() {
-            switch (_type) {
-            case ENGINEER:
-            return _monthlySalary;
-            case SALESMAN:
-            return _monthlySalary + _commission;
-            case MANAGER:
-            return _monthlySalary + _bonus;
-            default:
-            throw new RuntimeException("Incorrect Employee");
+        public static EmployeeType createType(int type) {
+            switch(type) {
+                case ENGINEER:
+                    return new Engineer();
+                case SALESMAN:
+                    return new SalesMan();
+                case MANAGER:
+                    return new Manager();
+                default:
+                    throw new IllegalArgumentException("Incorrect employee code type");
             }
         }
+        
+        abstract int payAmount();
     }
+    
+    static class Engineer extends EmployeeType {
+        private final int _monthlySalary;
+        
+        public Engineer() {
+            _monthlySalary = 199;
+        }
+        
+        @Override
+        public int payAmount() {
+            return _monthlySalary;
+        }
+    }
+    
+    static class Manager extends EmployeeType {
+        private final int _monthlySalary;
+        private final int _bonus;
+        
+        public Manager() {
+            _monthlySalary = 500;
+            _bonus = 1000;
+        }
+        
+        @Override
+        public int payAmount() {
+            return _monthlySalary + _bonus;
+        }
+    }
+    
+    static class SalesMan extends EmployeeType {
+        private final int _monthlySalary;
+        private final int _commission;
+        
+        public SalesMan() {
+            _monthlySalary = 1000;
+            _commission = 100;
+        }
+        
+        @Override
+        public int payAmount() {
+            return _monthlySalary + _commission;
+        }
+    }
+    
 }
