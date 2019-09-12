9953d2ce388c7ca6e312241cd1b8853544d66a1e
dev <dev@wsx09.mfti>
2019-09-12 15:59:10 +0200

    Encapsulate Field - refactoring

--- refactoring/src/main/java/refactoring/EncapsulateFieldExample.java
+++ refactoring/src/main/java/refactoring/EncapsulateFieldExample.java
@@ -1,6 +1,13 @@
 package refactoring;
 
 public class EncapsulateFieldExample {
-    public String _name;
+    private String _name;
 
+    public String getName() {
+        return _name;
+    }
+
+    public void setName(String name) {
+        _name = name;
+    }
 }
