024019cc075bd4d8a3a8d752330076ad88aae160
dev <dev@wsx09.mfti>
2019-09-12 15:57:16 +0200

    Introduce Local Extension - refactoring

--- refactoring/src/main/java/refactoring/IntroduceLocalExtensionExample.java
+++ refactoring/src/main/java/refactoring/IntroduceLocalExtensionExample.java
@@ -13,4 +13,19 @@ public class IntroduceLocalExtensionExample {
     public static Date nextDay(Date start) {
         return new Date(start.getYear(),start.getMonth(), start.getDate() + 1);
     }
+    
+    class dateSub extends Date {
+        
+        public dateSub(Date aDate) {
+            super(aDate.getTime());
+        }
+        
+        public dateSub(String dateString) {
+            super(dateString);
+        }
+        
+        public Date nextDay() {
+            return new Date(getYear(),getMonth(), getDate() + 1);
+        }
+    }
 }
